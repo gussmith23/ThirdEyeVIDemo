d6e42d8660834b61ea09874162da9ff0c71f78bb
Gus Smith <gus@justg.us>
2016-03-26 10:27:08 -0400

    adds event to framefetcher

--- ThirdEyeVI/WristbandCsharp/FrameFetcher.cs
+++ ThirdEyeVI/WristbandCsharp/FrameFetcher.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CAPIStreamServer;
 
 namespace WristbandCsharp
 {
     interface FrameFetcher
-    {
+    { 
+        event EventHandler FrameFetched;
+
         int Start();
         int Pause();
         int Stop();
