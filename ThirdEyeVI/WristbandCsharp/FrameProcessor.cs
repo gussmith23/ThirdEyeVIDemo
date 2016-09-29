1229b29315ae61ecb02ff20c5eef6c66c69ec5dc
ddema <ddema@DESKTOP-U7D9DCC>
2016-09-29 16:27:06 -0400

    FrameProcessor Changes and Include of WebcamFetcher

--- ThirdEyeVI/WristbandCsharp/FrameProcessor.cs
+++ ThirdEyeVI/WristbandCsharp/FrameProcessor.cs
@@ -8,6 +8,10 @@ namespace WristbandCsharp
 {
     class FrameProcessor: IFrameProcessor
     {
+        private List<string> itemsAvailableForLocation;
+        Tracker tracker = null;
+        CMTTracker cmtTracker = null;
+        SURFEngine testEngine = null;
         public event EventHandler<FrameProcessedEventArgs> FrameProcessed;
 
         protected virtual void OnFrameProcessed(FrameProcessedEventArgs e)
@@ -18,10 +22,27 @@ namespace WristbandCsharp
             }
         }
 
-        FrameProcessor(float width, float height)
+        public FrameProcessor(int trackType, Emgu.CV.Image<Emgu.CV.Structure.Bgr, Byte> itemImage)
         {
+            switch (trackType)
+            {
+                // CMT + SURF
+                case 0:
+                    tracker = new CMTTracker(itemImage);
+                    break;
+                case 1:
+                    throw new NotImplementedException("Write the code for Pure SURF tracking!");
+                default:
+                    tracker = null;
+                    break;
+            }
         }
-        public int Start()// Implement Framefetching. 
+        public FrameProcessor(int TrackType, string filename)
+        {
+            FrameProcessor(TrackType, new Emgu.CV.Image<Emgu.CV.Structure.Bgr, Byte>(filename));
+        }
+
+        public int Start()
         {
             return 0;
         }
@@ -32,6 +53,19 @@ namespace WristbandCsharp
         public int Stop()
         {
             return 0;
+        }
+
+        List<string> itemsAvailableTrack()
+        {
+            itemsAvailableForLocation = new List<string>();
+            string[] itemNames = System.IO.Directory.GetFiles("itemsToTrack/", "*.jpg");
+            foreach (string s in itemNames)
+            {
+                string name = System.Text.RegularExpressions.Regex.Replace(s, "itemsToTrack/", "");
+                name = System.Text.RegularExpressions.Regex.Replace(name, ".jpg", "");
+                itemsAvailableForLocation.Add(name);
+            }
+            return itemsAvailableForLocation;
         }
     }
 }
